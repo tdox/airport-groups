de3a4a3804b699cd11582214cc871726f10e23b1
Tad Doxsee <doxsee@pacbell.net>
2017-06-06 22:56:04 -0700

    cleanup

--- frontend/web-elm/airport-group-editor.elm
+++ frontend/web-elm/airport-group-editor.elm
@@ -19,7 +19,6 @@ main =
       , update = update
       , subscriptions = subscriptions
       }
---  Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
@@ -38,14 +37,6 @@ init initSrc initOutput =
     )
 
 
-    {-
-model : Model
-model =
-  { src = ""
-  , output = ""
-  }
--}
-
 
 -- UPDATE
 
@@ -61,48 +52,30 @@ update msg model =
       SaveSrc newSrc ->
         ({ model | src = newSrc}, Cmd.none)
           
---      Run  ->
---        { model | output = model.src}
-
       Run  ->
         (model, runProgram model.src)
             
       NewOutput (Err err) ->
---          ( model, Cmd.none)
           ( {model | output = toString err}, Cmd.none)
 
       NewOutput (Ok out) ->
           ( {model | output = String.concat(List.intersperse "\n" out.out)}, Cmd.none)
               
-          
-
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div [ centerStyle ]
-    [ -- button [ onClick Decrement ] [ text "-" ]
---      div [] [ text model.src ]
---      input [ placeholder "Enter group definitions here", onInput Interpret ]  []
+    [
       h1 [h1Style] [text "Airport Groups"]
     , textarea [ srcStyle, placeholder "Enter group definitions here", onInput SaveSrc ]  []
     , p [] []
     , button [ style [fontSize], onClick Run ] [ text "Run" ]
     , p [] []
     , textarea [outStyle, disabled True] [ text model.output]
-    -- , div [textStyle] [ text model.output]
-    --, button [ onClick Increment ] [ text "+" ]
     ]
 
--- textarea : List (Attribute msg) -> List (Html msg) -> Html msg
--- input : List (Attribute msg) -> List (Html msg) -> Html msg
--- button :  List (Attribute msg) -> List (Html msg) -> Html msg
--- type alias Attribute msg = Property msg
--- text : String -> Html msg
--- onInput : (String -> msg) -> Attribute msg
--- placeholder : String -> Attribute msg
--- style : List (String, String) -> Attribute msg
 
 -- SUBSCRIPTIONS
 
@@ -110,37 +83,31 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
+      
 -- HTTP
-
 
 mkSourceCode : String -> SourceCode
 mkSourceCode src = SourceCode src
 
 encodeSourceCode : SourceCode -> Value
 encodeSourceCode sc  = object [("src", Json.Encode.string sc.src)]
---encodeSourceCode sc  = Json.Encode.string sc.src
 
 mkBody : String -> Body
 mkBody src = jsonBody (encodeSourceCode (mkSourceCode src))
 
 progOutputDecoder : Decoder ProgOutput
 progOutputDecoder = map ProgOutput (field "out" (Json.Decode.list Json.Decode.string))
---progOutputDecoder = decodeString (field "out" (list string))
---progOutputDecoder = ProgOutput (field "out" (list string))
-
 
 runProgram : String -> Cmd Msg
 runProgram src =
     let
         url = "http://localhost:8080/airport-group"
         req = Http.post url (mkBody src) progOutputDecoder
-        reqStr = log "req" req
+        --reqStr = log "req" req
     in
         Http.send NewOutput req
---        Http.send NewOutput (Http.post url (mkBody src) progOutputDecoder)
 
--- send : (Result Error a -> msg) -> Request a -> Cmd msg
--- post : 
+
       
 -- Styles
 
@@ -160,7 +127,6 @@ outStyle =
     , ("height", "300px")
     , ("width", "80%")
     , fontSize
---    , ("contenteditable", "false")
     ]
 
 textStyle : Attribute msg
@@ -176,7 +142,6 @@ fontSize = ("font-size", "14px")
 h1Style =
   style
     [ ("font-family", "sans-serif")
---    , ("text-align", "left")
     ]
 
 centerStyle = style [("text-align", "center")]
